2440b05eca94fb28b72f973a1a648d13b5a1fc75
Ellis Clayton <ellis@ellis.codes>
2019-12-06 09:13:03 +1100

    [WIP] Add deploy stage

--- pass/cmd/main.go
+++ pass/cmd/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/docker/docker-credential-helpers/credentials"
-	"github.com/docker/docker-credential-helpers/pass"
+	"github.com/ellsclytn/docker-credential-helpers/credentials"
+	"github.com/ellsclytn/docker-credential-helpers/pass"
 )
 
 func main() {
--- pass/pass.go
+++ pass/pass.go
@@ -16,7 +16,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/docker/docker-credential-helpers/credentials"
+	"github.com/ellsclytn/docker-credential-helpers/credentials"
 )
 
 const PASS_FOLDER = "docker-credential-helpers"
